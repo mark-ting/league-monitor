382836e4243a12352bd108ba5a36df4f8827c58e
richkcho <richkcho19@gmail.com>
2015-01-02 00:42:31 -0800

    new base android app to make it workerino
    plzerino

--- League_Monitor_App/MyApplication/app/src/androidTest/java/com/example/codingslash/leaguemonitorapp/ApplicationTest.java
+++ League_Monitor_App/MyApplication/app/src/androidTest/java/com/example/codingslash/leaguemonitorapp/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.codingslash.leaguemonitor;
+package com.example.codingslash.leaguemonitorapp;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- League_Monitor_App/MyApplication/app/src/main/java/com/example/codingslash/leaguemonitorapp/MainActivity.java
+++ League_Monitor_App/MyApplication/app/src/main/java/com/example/codingslash/leaguemonitorapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.codingslash.leaguemonitor;
+package com.example.codingslash.leaguemonitorapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
